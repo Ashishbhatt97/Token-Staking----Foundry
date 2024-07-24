8f73039516d2053b8e9a25b96bfd8f7208625f2e
Ashishbhatt97 <ashishbhatt0197@gmail.com>
2024-07-24 18:35:49 +0530

    Staking contract done

--- src/Staking.sol
+++ src/Staking.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract Staking is ReentrancyGuard {
-    using SafeMath for uint256;
     IERC20 public stakingToken;
     IERC20 public rewardToken;
 
